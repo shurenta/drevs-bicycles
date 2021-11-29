8d42e6c06917734c86b136edaf82bc5b8ef0ea0a
Alexander <drevs.aleksandr@gmail.com>
2021-11-29 10:32:34 +0600

    Перенос

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,19 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+if(document.querySelector('.main-nav')) {
+  var mainNav = document.querySelector('.main-nav');
+  var buttonToggle = document.querySelector('.toogle-nav');
+  mainNav.classList.remove('main-nav--nojs');
+  buttonToggle.classList.remove('toogle-nav--nojs');
+  buttonToggle.addEventListener('click', function() {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+      buttonToggle.classList.remove('toogle-nav--opened');
+      buttonToggle.classList.add('toogle-nav--closed');
+    } else {
+        mainNav.classList.add('main-nav--closed');
+        mainNav.classList.remove('main-nav--opened');
+        buttonToggle.classList.add('toogle-nav--opened');
+        buttonToggle.classList.remove('toogle-nav--closed');
+      }
+    });
+}
