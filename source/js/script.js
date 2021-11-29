4a4d03453b4324d029dfe234baf7b48c79252765
alexsandr <drevs.aleksandr@gmail.com>
2021-11-29 16:29:15 +0600

    тест

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,14 @@
-if(document.querySelector('.main-nav')) {
+if(document.querySelector('.page-header')) {
   var mainNav = document.querySelector('.main-nav');
   var buttonToggle = document.querySelector('.toogle-nav');
+  var headerImage = document.querySelector('.background-img');
   mainNav.classList.remove('main-nav--nojs');
   buttonToggle.classList.remove('toogle-nav--nojs');
   buttonToggle.addEventListener('click', function() {
     if (mainNav.classList.contains('main-nav--closed')) {
       mainNav.classList.remove('main-nav--closed');
       mainNav.classList.add('main-nav--opened');
+      headerImage.classList.add('background-img--opened');
       buttonToggle.classList.remove('toogle-nav--opened');
       buttonToggle.classList.add('toogle-nav--closed');
     } else {
@@ -14,6 +16,7 @@ if(document.querySelector('.main-nav')) {
         mainNav.classList.remove('main-nav--opened');
         buttonToggle.classList.add('toogle-nav--opened');
         buttonToggle.classList.remove('toogle-nav--closed');
+        headerImage.classList.remove('background-img--opened');
       }
     });
 }
